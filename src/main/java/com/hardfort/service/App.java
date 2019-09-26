021f3610bbfcb7589994d5e1299b0dd0a2aefd58
CPG7358 <CPG7358@HFDVDI018857.ad1.prod>
2019-09-26 12:59:46 +0530

    revove line

--- src/main/java/com/hardfort/service/App.java
+++ src/main/java/com/hardfort/service/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Hello World! by java8" );
-        System.out.println("Hello welcome by java8" );
         System.out.println("Hello welcome Spring" );
     }
 }
